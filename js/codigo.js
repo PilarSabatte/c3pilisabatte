4709fbf11a066571a7b6bda1b1cd95c65b4a8713
PilarSabatte <136595240+PilarSabatte@users.noreply.github.com>
2023-06-14 11:22:10 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,20 @@
+document.getElementById('menu').addEventListener('click', function () {
+    document.getElementById('navega').classList.toggle('mostrar');
+})
+
+lightbox.option({
+    'wrapAround': true,
+    'albumLabel': "Imagen %1 de %2",
+    'fadeDuration': 500,
+    'maxWidth': 50,
+    'maxHeight': 50
+})
+
+document.getElementById('boton').addEventListener('click', function () {
+  if(document.getElementById('miVideo').paused)
+   document.getElementById('miVideo').play();
+else
+   document.getElementById('miVideo').pause();
+
+})
+
